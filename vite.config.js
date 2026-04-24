e27df4e684eb98dffe7ac84bed21c492e6c44473
Simon Vedder <info@simon-vedder.de>
2026-04-24 18:35:38 +0200

    Update: to root path for custom domain

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: "/least-privilege-studio/",
+  base: "/",
   build: {
     outDir: "dist",
   },
